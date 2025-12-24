ebf6951a26d9a3182840729577069c2057a2c7e5
Orlando Junior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-12-23 21:41:43 -0300

    fix: ajusta Configuração deploy no GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'site_adv' // Nome do seu repositório no GitHub
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}` : '',
 }
 
 module.exports = nextConfig
